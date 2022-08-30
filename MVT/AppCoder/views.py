5ee5ecdca329a1d0cdc1a4c343adbb66b70c1bdb
IgnacioGuerrero <Guerrero.ignacio.joaquin@gmail.com>
2022-08-30 11:04:40 -0300

    tercer commit

--- MVT/AppCoder/views.py
+++ MVT/AppCoder/views.py
@@ -5,15 +5,15 @@ from django.template import loader
 # Create your views here.
 
 def familiares(request):
-    familiar=Familiares(nombre='Augusto', dni=40123321,cumpleaños='1999-05-22')
+    familiar=Familiares(nombre='Mercedez', dni=17000021,cumpleaños='1984-08-22')
     familiar.save()
-    familiar1=Familiares(nombre='Juan', dni=19449990,cumpleaños='1945-07-21')
+    familiar1=Familiares(nombre='Carlos', dni=18567298,cumpleaños='1973-07-21')
     familiar1.save()
-    familiar2=Familiares(nombre='Augusto', dni=40123321,cumpleaños='1969-01-14')
+    familiar2=Familiares(nombre='Augusto', dni=32567432,cumpleaños='1965-12-12')
     familiar2.save()
     diccionario={'Nombre':familiar.nombre,'Dni':familiar.dni,'Cumpleaños':familiar.cumpleaños,
                 'Nombre1':familiar1.nombre,'Dni1':familiar1.dni,'Cumpleaños1':familiar1.cumpleaños,
-                'Nombre2':familiar.nombre,'Dni2':familiar2.dni,'Cumpleaños2':familiar2.cumpleaños
+                'Nombre2':familiar2.nombre,'Dni2':familiar2.dni,'Cumpleaños2':familiar2.cumpleaños
                 }
     plantilla=loader.get_template('template1.html')
     documento=plantilla.render(diccionario)
