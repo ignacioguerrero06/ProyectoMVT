cff44489ed9c6eafeb9329aec7db720a2f87005f
IgnacioGuerrero <Guerrero.ignacio.joaquin@gmail.com>
2022-08-30 10:20:23 -0300

    segundo commit

--- MVT/AppCoder/views.py
+++ MVT/AppCoder/views.py
@@ -7,7 +7,14 @@ from django.template import loader
 def familiares(request):
     familiar=Familiares(nombre='Augusto', dni=40123321,cumpleaños='1999-05-22')
     familiar.save()
-    diccionario={'Nombre':familiar.nombre,'Dni':familiar.dni,'Cumpleaños':familiar.cumpleaños}
+    familiar1=Familiares(nombre='Juan', dni=19449990,cumpleaños='1945-07-21')
+    familiar1.save()
+    familiar2=Familiares(nombre='Augusto', dni=40123321,cumpleaños='1969-01-14')
+    familiar2.save()
+    diccionario={'Nombre':familiar.nombre,'Dni':familiar.dni,'Cumpleaños':familiar.cumpleaños,
+                'Nombre1':familiar1.nombre,'Dni1':familiar1.dni,'Cumpleaños1':familiar1.cumpleaños,
+                'Nombre2':familiar.nombre,'Dni2':familiar2.dni,'Cumpleaños2':familiar2.cumpleaños
+                }
     plantilla=loader.get_template('template1.html')
     documento=plantilla.render(diccionario)
     return HttpResponse(documento)
